dd8df963fcb92c1589629b6b1df1be4e2133b2c1
LepkoQQ <lepko.san@gmail.com>
2019-05-12 22:43:25 +0200

    update vps version

--- dist/video-player-scroller.user.js
+++ dist/video-player-scroller.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name        Video Player Scroller
 // @namespace   http://poglej.ga/
-// @version     4.1.0
+// @version     4.1.1
 // @run-at      document-start
 // @include     *
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/utils/utils.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/utils/reacthook.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/video-player-scroller/_videoscroller.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/video-player-scroller/vimeo.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/video-player-scroller/twitch.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/e95b498c114759d0ad4e9458c6d75748971e5955/dist/video-player-scroller/youtube.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/utils/utils.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/utils/reacthook.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/video-player-scroller/_videoscroller.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/video-player-scroller/vimeo.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/video-player-scroller/twitch.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/25f8c9a387d477391459d6800f564f7320af3a72/dist/video-player-scroller/youtube.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
