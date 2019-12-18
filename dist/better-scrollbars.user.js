c53a5108669abb601d4805844626c978bc39a658
LepkoQQ <lepko.san@gmail.com>
2019-12-18 21:05:18 +0100

    add better scrollbars script

---
+++ dist/better-scrollbars.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Better Scrollbars
+// @namespace   http://poglej.ga/
+// @version     1.0.0
+// @run-at      document-start
+// @include     *
+// @grant       GM_addStyle
+// @nocompat    Chrome
+// ==/UserScript==
+
+/* global GM_addStyle:false */
+GM_addStyle(`
+  :root {
+    --custom-better-scrollbar-color: rgba(142, 142, 142, 0.65);
+    --custom-better-scrollbar-color-hover: rgba(142, 142, 142, 0.85);
+  }
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--custom-better-scrollbar-color);
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: var(--custom-better-scrollbar-color-hover);
+  }
+`);
