671848e5a36cc2407a9844df27843c7dc149627c
LepkoQQ <lepko.san@gmail.com>
2023-03-12 15:00:55 +0100

    include patreon wide in script

--- dist/video-player-scroller/patreon.js
+++ dist/video-player-scroller/patreon.js
@@ -1,5 +1,5 @@
 /* global _:false, VideoScroller:false */
-(function dropbox(context, window) {
+(function patreon(context, window) {
   'use strict';
 
   let LOGGER;
@@ -29,6 +29,50 @@
     }
   }
 
+  function widenPosts() {
+    if (!document.body) {
+      return;
+    }
+
+    let treeWalker;
+    let currentNode;
+
+    // page container
+    treeWalker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_ELEMENT,
+      (node) => {
+        const cs = window.getComputedStyle(node);
+        if (cs['max-width'] === '1152px' || cs['max-width'] === '984px') {
+          return NodeFilter.FILTER_ACCEPT;
+        }
+        return NodeFilter.FILTER_SKIP;
+      },
+      false
+    );
+    while ((currentNode = treeWalker.nextNode())) {
+      currentNode.style['max-width'] = '90rem';
+    }
+
+    // main post column
+    treeWalker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_ELEMENT,
+      (node) => {
+        const cs = window.getComputedStyle(node);
+        if (cs['max-width'] === '66.6667%' && cs['flex-basis'] === '66.6667%') {
+          return NodeFilter.FILTER_ACCEPT;
+        }
+        return NodeFilter.FILTER_SKIP;
+      },
+      false
+    );
+    while ((currentNode = treeWalker.nextNode())) {
+      currentNode.style['max-width'] = '100%';
+      currentNode.style['flex-basis'] = '100%';
+    }
+  }
+
   if (context.vpsSite == null && window.location.host.match(/(?:\.|^)patreon\.com$/)) {
     context.vpsSite = {
       init(logger) {
@@ -36,11 +80,15 @@
         scrollerOptions.logger = LOGGER;
         LOGGER.log('started', window.location);
 
-        _.waitFor(() => _.get('[data-tag="post-card"] video'))
-          .then((video) => {
-            videoFound(video);
-          });
+        _.waitFor(() => _.get('[data-tag="post-card"] video')).then((video) => {
+          videoFound(video);
+        });
       },
     };
+
+    widenPosts();
+    document.addEventListener('readystatechange', () => {
+      widenPosts();
+    });
   }
-}(this, this.unsafeWindow || window));
+})(this, this.unsafeWindow || window);
