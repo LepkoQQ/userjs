5522045482078c89234d7af4da8d414dca24248d
LepkoQQ <lepko.san@gmail.com>
2023-03-27 16:46:41 +0200

    update better scrollbars to v2

--- dist/better-scrollbars.user.js
+++ dist/better-scrollbars.user.js
@@ -1,34 +1,44 @@
 // ==UserScript==
-// @name        Better Scrollbars
-// @namespace   http://poglej.ga/
-// @version     1.0.1
-// @run-at      document-start
-// @include     *
-// @grant       GM_addStyle
-// @nocompat    Chrome
+// @name         Better Scrollbars
+// @namespace    http://poglej.ga/
+// @version      2.0.0
+// @run-at       document-start
+// @match        *://*/*
+// @grant        GM_addStyle
+// @nocompat     Chrome
 // ==/UserScript==
 
 /* global GM_addStyle:false */
 GM_addStyle(`
-  :root {
-    --custom-better-scrollbar-color: rgba(142, 142, 142, 0.65);
-    --custom-better-scrollbar-color-hover: rgba(142, 142, 142, 0.85);
-  }
+:root {
+  --_cbsb-background: rgba(142, 142, 142, 0.25);
+  --_cbsb-color: rgba(142, 142, 142, 0.65);
+  --_cbsb-color-hover: rgba(142, 142, 142, 0.85);
+}
 
-  ::-webkit-scrollbar {
-    width: 6px;
-    height: 6px;
-  }
+::-webkit-scrollbar {
+  width: 10px !important;
+  height: 10px !important;
+  background-color: transparent !important;
+}
 
-  ::-webkit-scrollbar-corner {
-    background: transparent;
-  }
+::-webkit-scrollbar-track-piece {
+  background-color: var(--_cbsb-background) !important;
+}
 
-  ::-webkit-scrollbar-thumb {
-    background-color: var(--custom-better-scrollbar-color);
-  }
+::-webkit-scrollbar-corner {
+  background-color: var(--_cbsb-background) !important;
+}
 
-  ::-webkit-scrollbar-thumb:hover {
-    background-color: var(--custom-better-scrollbar-color-hover);
-  }
+::-webkit-scrollbar-thumb {
+  height: 48px !important;
+  border: 2px solid transparent !important;
+  border-radius: 0 !important;
+  background-clip: content-box !important;
+  background-color: var(--_cbsb-color) !important;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: var(--_cbsb-color-hover) !important;
+}
 `);
