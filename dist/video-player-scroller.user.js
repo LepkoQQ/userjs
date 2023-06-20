7188b931ba813c57549556235c55c2b4c6294f9c
LepkoQQ <lepko.san@gmail.com>
2023-06-20 17:49:50 +0200

    add bunnycdn

--- dist/video-player-scroller.user.js
+++ dist/video-player-scroller.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name        Video Player Scroller
+// @description Add more controls to video players
 // @namespace   http://poglej.ga/
-// @version     4.4.2
-// @run-at      document-start
-// @include     *
+// @version     4.4.3
+// @run-at      document-idle
+// @match       *://*/*
 // @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/utils/utils.js
 // @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/utils/reacthook.js
 // @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/_videoscroller.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/vimeo.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/twitch.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/youtube.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/bunnycdn.js
 // @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/dropbox.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/vidyard.js
 // @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/patreon.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/twitch.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/vidyard.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/vimeo.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/ca9ab2898c782a0efc89f951b6d142bd158c7256/dist/video-player-scroller/youtube.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
---
+++ dist/video-player-scroller/bunnycdn.js
@@ -0,0 +1,60 @@
+/* global _:false, VideoScroller:false */
+(function bunnycdn(context, window) {
+  'use strict';
+
+  let LOGGER;
+  let videoScroller;
+
+  const scrollerOptions = {
+    color: '#1261fe',
+    getBottomOffset() {
+      return 55;
+    },
+    getLeftOffset() {
+      return 10;
+    },
+  };
+
+  function playerLoaded(player) {
+    if (videoScroller) {
+      videoScroller.destroy();
+      videoScroller = null;
+    }
+    if (!videoScroller) {
+      videoScroller = new VideoScroller(player, scrollerOptions);
+    }
+  }
+
+  if (context.vpsSite == null && ['video.bunnycdn.com', 'iframe.mediadelivery.net'].includes(window.location.host)) {
+    context.vpsSite = {
+      init(logger) {
+        LOGGER = logger.push('bunnycdn');
+        scrollerOptions.logger = LOGGER;
+        LOGGER.log('started', window.location);
+
+        if (window.location.pathname.startsWith('/play/')) {
+          _.addCSS(`
+            #body #video-wrapper .container {
+              position: fixed;
+              inset: 0;
+              margin: 0;
+              padding-top: 0;
+            }
+          `);
+        }
+
+        if (window.location.pathname.startsWith('/embed/')) {
+          _.addCSS(`
+            #body #video-container {
+              background: #000;
+            }
+          `);
+
+          _.waitFor(() => _.get('.plyr video')).then((player) => {
+            playerLoaded(player.parentElement.parentElement);
+          });
+        }
+      },
+    };
+  }
+})(this, this.unsafeWindow || window);
