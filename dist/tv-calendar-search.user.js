de725193edb326e484c3a857671366109796bf69
LepkoQQ <lepko.san@gmail.com>
2023-07-06 18:42:45 +0200

    bump tv search version

--- dist/tv-calendar-search.user.js
+++ dist/tv-calendar-search.user.js
@@ -2,14 +2,14 @@
 // @name        TV Calendar Search
 // @description Add search links to calendar
 // @namespace   http://lepko.net/
-// @version     2.0.0
+// @version     2.0.1
 // @run-at      document-idle
 // @match       *://*.pogdesign.co.uk/cat/*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.0.2/purify.min.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/7188b931ba813c57549556235c55c2b4c6294f9c/dist/utils/utils.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/7188b931ba813c57549556235c55c2b4c6294f9c/dist/tv-calendar-search/_asyncmenu.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/7188b931ba813c57549556235c55c2b4c6294f9c/dist/tv-calendar-search/api.js
-// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/7188b931ba813c57549556235c55c2b4c6294f9c/dist/tv-calendar-search/main.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/012db8d2ec041668878722807f9a70e888f9db02/dist/utils/utils.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/012db8d2ec041668878722807f9a70e888f9db02/dist/tv-calendar-search/_asyncmenu.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/012db8d2ec041668878722807f9a70e888f9db02/dist/tv-calendar-search/api.js
+// @require     https://raw.githubusercontent.com/LepkoQQ/userjs/012db8d2ec041668878722807f9a70e888f9db02/dist/tv-calendar-search/main.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
