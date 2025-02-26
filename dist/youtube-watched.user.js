d4011aa56a90321e605b06cd788400d23f7ac03d
LepkoQQ <lepko.san@gmail.com>
2025-02-26 22:11:18 +0100

    update version hash url

--- dist/youtube-watched.user.js
+++ dist/youtube-watched.user.js
@@ -6,8 +6,8 @@
 // @author       You
 // @run-at       document-start
 // @match        https://www.youtube.com/*
-// @require      https://raw.githubusercontent.com/LepkoQQ/userjs/xxx/dist/utils/utils.js
-// @require      https://raw.githubusercontent.com/LepkoQQ/userjs/xxx/dist/youtube-watched/main.js
+// @require      https://raw.githubusercontent.com/LepkoQQ/userjs/f940b86a2ce17f596f09177f188df2148a9bc554/dist/utils/utils.js
+// @require      https://raw.githubusercontent.com/LepkoQQ/userjs/f940b86a2ce17f596f09177f188df2148a9bc554/dist/youtube-watched/main.js
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // ==/UserScript==
